490c90974719c3357e6865ad919870b07a9a878a
Sofyan Sugianto <sofyanartem@gmail.com>
2021-06-26 17:42:21 +0700

    membuat rute halaman

---
+++ lib/config/routes.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:pencatat_keuangan/config/constant.dart';
+import 'package:pencatat_keuangan/screens/dashboard_screen.dart';
+import 'package:pencatat_keuangan/screens/firstrun_screen.dart';
+import 'package:pencatat_keuangan/screens/new_transaction_screen.dart';
+
+class Routes {
+  Route<dynamic>? onGenerateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case dashboardPage:
+        return MaterialPageRoute(
+            settings: settings, builder: (_) => DashboardPage());
+      case firstRunPage:
+        return MaterialPageRoute(
+            settings: settings, builder: (_) => FirstRunPage());
+      case addNewTransactionPage:
+        return MaterialPageRoute(
+            settings: settings, builder: (_) => NewTransactionScreen());
+      default:
+        return null;
+    }
+  }
+}
