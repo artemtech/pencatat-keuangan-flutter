be6f2f4836d79eee74b673dd5aba416545e7888b
Sofyan Sugianto <sofyanartem@gmail.com>
2021-06-26 17:38:24 +0700

    menambahkan pustaka yang dibutuhkan.
    - Hive untuk menyimpan data secara local
    - intl untuk formatting

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:sofyan_pos/main.dart';
+import 'package:pencatat_keuangan/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
