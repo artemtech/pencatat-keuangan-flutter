fb9ad9112dd4cd849999e7eda29195c759312260
Sofyan Sugianto <sofyanartem@gmail.com>
2021-06-26 17:51:20 +0700

    menginisiasi Hive saat aplikasi dijalankan

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:hive/hive.dart';
 import 'package:pencatat_keuangan/config/constant.dart';
 import 'package:pencatat_keuangan/config/routes.dart';
+import 'package:pencatat_keuangan/models/transaction.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:path_provider/path_provider.dart' as pathProvider;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  var appDocumentDirectory =
+      await pathProvider.getApplicationDocumentsDirectory();
+  Hive.init(appDocumentDirectory.path);
+  Hive.registerAdapter(TransactionAdapter());
+  Hive.registerAdapter(TransactionTypeAdapter());
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
       .then((value) => runApp(MyApp()));
 }
