618d4c91df2544e9888ecb1ce945cafb161a6361
Sofyan Sugianto <sofyanartem@gmail.com>
2021-06-29 19:40:22 +0700

    bikin nav drawer

--- lib/config/constant.dart
+++ lib/config/constant.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:pencatat_keuangan/components/appdrawer.dart';
 
 // named route
 const String dashboardPage = '/dashboard';
 const String firstRunPage = '/firstRun';
 const String addNewTransactionPage = '/transaction/new';
+const String reportPage = '/report';
 
 // styles
 const Color blackTextColor = Color(0xFF334455);
-const Color whiteTextColor = Color(0xFFCDD8E3);
+const Color whiteTextColor = Color(0xffffffff);
+const Color whiteShadeColor = Color(0xffcdd8e3);
 const Color primarySolid = Color(0xffad1457);
 
 ThemeData appTheme = ThemeData(
@@ -20,3 +23,5 @@ TextStyle textStyle({Color textColor = Colors.green, bool isBold = false}) {
       color: textColor,
       fontWeight: isBold ? FontWeight.bold : FontWeight.normal);
 }
+
+AppDrawer navDrawer = AppDrawer();
