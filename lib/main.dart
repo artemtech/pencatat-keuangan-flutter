9e3ebf9670526ce4d6f706b05735483ebc04d035
Sofyan Sugianto <sofyanartem@gmail.com>
2021-06-29 19:46:07 +0700

    inisialisasi report adapter di file main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hive/hive.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:pencatat_keuangan/config/constant.dart';
 import 'package:pencatat_keuangan/config/routes.dart';
+import 'package:pencatat_keuangan/models/report.dart';
 import 'package:pencatat_keuangan/models/transaction.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:path_provider/path_provider.dart' as pathProvider;
@@ -14,6 +16,8 @@ void main() async {
   Hive.init(appDocumentDirectory.path);
   Hive.registerAdapter(TransactionAdapter());
   Hive.registerAdapter(TransactionTypeAdapter());
+  Hive.registerAdapter(ReportAdapter());
+  initializeDateFormatting('id_ID');
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
       .then((value) => runApp(MyApp()));
 }
